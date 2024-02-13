5b735d25f8b798398b7df72930ba4b9b6f8b8b63
Nicholas Young <91038131+nickyoung92@users.noreply.github.com>
2024-02-13 17:30:16 -0500

    Update PaymentSplitter.sol

--- PaymentSplitter.sol
+++ PaymentSplitter.sol
@@ -12,7 +12,7 @@ All payee addresses must be able to receive ETH, otherwise it will revert for ev
 Anyone can call withdraw function. Withdraw function will withdraw entire contract balance and split according to shares/totalshares.
 
 */
-contract PaymentSplitter is Ownable {
+abstract contract PaymentSplitter is Ownable {
     IERC20 internal tokenContract;
     address payable[] public payees;
     uint256[] public shares;
